96fbda1c9ef7e382e4d578b899a9021f3cec78aa
Provashish Roy <provashishroy95@gmail.com>
2022-05-19 00:34:12 +0600

    added home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Home = () => {
+    const nameRef = useRef('');
+    const descriptionRef = useRef('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const description = descriptionRef.current.value;
+        console.log(name, description);
+    }
+
+
     return (
-        <div>
-            <h1>this is home page</h1>
+        <div className='container mt-3 '>
+            <div className='w-50 mx-auto '>
+                <h4 className='text-center text-dark fw-bold mb-4'>Add Task</h4>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Control ref={nameRef} name='name' type="text" placeholder="Enter Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Control ref={descriptionRef} as="textarea" rows={3} placeholder="Description" />
+                    </Form.Group>
+                    <Button className='w-50 mx-auto d-block mb-2' variant="dark" type="submit">
+                        Add
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
