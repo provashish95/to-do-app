a5bd19feb14f87bd797be74c2d2cf2a5f5f0a644
Provashish Roy <provashishroy95@gmail.com>
2022-05-19 00:12:54 +0600

    added login system

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,67 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Home/Loading';
 import SocialLogin from './SocialLogin';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const emailRef = useRef('');
     const passwordRef = useRef('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(auth);
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+
+
+    useEffect(() => {
+        if (user) {
+            navigate(from, { replace: true });
+            navigate('/home');
+        }
+    }, [user, navigate, from])
+
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password);
+        toast('Login Successfully');
+    }
+    if (error || resetError) {
+        errorElement = <p className='text-danger'>Error: {error?.message}  {resetError?.message} </p>
+    }
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast.success('Sent email');
+        } else {
+            toast.error('Please enter your email');
+        }
+    }
+
     return (
-        <div className='container mt-5'>
-            <div className='w-50 mx-auto'>
-                <h5 className='text-center text-dark fw-bold mb-4'>Login</h5>
-                <Form >
+        <div className='container mt-3 mb-5 '>
+            <div className='w-50 mx-auto '>
+                <h4 className='text-center text-dark fw-bold mb-4'>Login</h4>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Control ref={emailRef} name='email' type="email" placeholder="Enter email" required />
                     </Form.Group>
@@ -23,15 +73,21 @@ const Login = () => {
                     </Button>
                 </Form>
                 {
-                    //errorElement
+                    errorElement
                 }
-                <p>Forget Password?<span className='btn text-dark fst-italic fw-bold'>Reset Password</span></p>
+                <p>Forget Password?<span className='btn text-dark fst-italic fw-bold' onClick={resetPassword}>Reset Password</span></p>
 
                 <p>New to gym master?<span className='btn text-dark fst-italic fw-bold' onClick={() => navigate('/register')} >Please Register </span></p>
                 <SocialLogin></SocialLogin>
+                <ToastContainer toastStyle={{
+                    backgroundColor: "rgb(216, 216, 216)",
+                    marginTop: "4rem",
+                    color: "black",
+                    borderRadius: "20px"
+                }} />
             </div>
 
-        </div >
+        </div>
     );
 };
 
